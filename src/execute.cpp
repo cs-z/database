1628ab7956be0ee103eeaed63cbb0fa0af723306
Zalán Csepeli <zalan.csepeli@gmail.com>
2025-07-27 15:24:43 +0200

    report query rows and duration

--- src/execute.cpp
+++ src/execute.cpp
@@ -77,7 +77,25 @@ static std::string pad(const std::string &string, size_t width, bool left)
 
 static void execute_query(const Query &query)
 {
+	const auto time_start = std::chrono::high_resolution_clock::now();
 	query.iter->open();
+
+	std::vector<Value> values;
+	unsigned int count = 0;
+	while (!query.limit || count < *query.limit) {
+		const std::optional<Value> value = query.iter->next();
+		if (value) {
+			values.push_back(std::move(*value));
+			count++;
+		}
+		else {
+			break;
+		}
+	}
+
+	query.iter->close();
+	const auto time_end = std::chrono::high_resolution_clock::now();
+	const std::chrono::duration<double, std::milli> time_delta = time_end - time_start;
 
 	std::vector<std::vector<std::string>> rows;
 	std::vector<size_t> max_sizes;
@@ -86,19 +104,11 @@ static void execute_query(const Query &query)
 		max_sizes.push_back(name.size());
 	}
 
-	unsigned int count = 0;
-	while (!query.limit || count < *query.limit) {
-		const std::optional<Value> value = query.iter->next();
-		if (value) {
-			count++;
-		}
-		else {
-			break;
-		}
-		ASSERT(value->size() == query.table_def.size());
+	for (const Value &value : values) {
+		ASSERT(value.size() == query.table_def.size());
 		std::vector<std::string> strings;
 		for (size_t i = 0; i < query.table_def.size(); i++) {
-			std::string string = column_value_to_string(value->at(i), false);
+			std::string string = column_value_to_string(value[i], false);
 			max_sizes[i] = std::max(max_sizes[i], string.size());
 			strings.push_back(std::move(string));
 		}
@@ -107,77 +117,37 @@ static void execute_query(const Query &query)
 
 	for (size_t i = 0; i < query.table_def.size(); i++) {
 		const std::string string(max_sizes[i] + 2, '-');
-		printf("+%s", string.c_str());
+		std::printf("+%s", string.c_str());
 	}
-	printf("+\n");
+	std::printf("+\n");
 
 	for (size_t i = 0; i < query.table_def.size(); i++) {
 		const std::string string = pad(query.table_def[i].name, max_sizes[i], true);
-		printf("| %s ", string.c_str());
+		std::printf("| %s ", string.c_str());
 	}
-	printf("|\n");
+	std::printf("|\n");
 
 	for (size_t i = 0; i < query.table_def.size(); i++) {
 		const std::string string(max_sizes[i] + 2, '-');
-		printf("+%s", string.c_str());
+		std::printf("+%s", string.c_str());
 	}
-	printf("+\n");
+	std::printf("+\n");
 
 	for (const std::vector<std::string> &row : rows) {
 		for (size_t i = 0; i < query.table_def.size(); i++) {
 			const std::string string = pad(row[i], max_sizes[i], !column_type_is_arithmetic(query.table_def[i].type));
-			printf("| %s ", string.c_str());
+			std::printf("| %s ", string.c_str());
 		}
-		printf("|\n");
+		std::printf("|\n");
 	}
 
 	for (size_t i = 0; i < query.table_def.size(); i++) {
 		const std::string string(max_sizes[i] + 2, '-');
-		printf("+%s", string.c_str());
+		std::printf("+%s", string.c_str());
 	}
-	printf("+\n");
+	std::printf("+\n");
 
-	/*unsigned int count = 0;
-	static constexpr u32 WIDTH = 16;
-	printf("|");
-	for (const auto &[column_name, column_type] : query.table_def) {
-		const std::string string = column_name;
-		const unsigned int pad = WIDTH - std::min(WIDTH, (u32) string.size());
-		for (unsigned int i = 0; i < pad - pad / 2; i++) printf(" ");
-		printf("%s", string.c_str());
-		for (unsigned int i = 0; i < pad / 2; i++) printf(" ");
-		printf("|");
-	}
-	printf("\n");
-	printf("|");
-	for (const auto &[column_name, column_type] : query.table_def) {
-		(void) column_name;
-		(void) column_type;
-		const unsigned int pad = WIDTH;
-		for (unsigned int i = 0; i < pad; i++) printf("-");
-		printf("|");
-	}
-	printf("\n");
-
-	for (;;) {
-		const std::optional<Value> value = iter->next();
-		if (!value) {
-			break;
-		}
-		count++;
-		printf("|");
-		for (const ColumnValue &column_value : *value) {
-			const std::string string = column_value_to_string(column_value, false);
-			const unsigned int pad = WIDTH - std::min(WIDTH, (u32) string.size());
-			for (unsigned int i = 0; i < pad - pad / 2; i++) printf(" ");
-			printf("%s", string.c_str());
-			for (unsigned int i = 0; i < pad / 2; i++) printf(" ");
-			printf("|");
-		}
-		printf("\n");
-	}*/
-
-	query.iter->close();
+	std::printf("(%u rows in %.1lf ms)\n", count, time_delta.count());
 }
 
 void execute_statement(const Statement &statement)
@@ -197,12 +167,8 @@ void execute_statement(const Statement &statement)
 
 /*void execute_query(const std::string &source)
 {
-	const auto time_start = std::chrono::high_resolution_clock::now();
 
-	const auto time_end = std::chrono::high_resolution_clock::now();
-	const std::chrono::duration<double, std::milli> time_delta = time_end - time_start;
-
-	printf("rows: %u\n", count);
-	printf("time: %.1lf ms\n", time_delta.count());
-	printf("\n");
+	std::printf("rows: %u\n", count);
+	std::printf("time: %.1lf ms\n", time_delta.count());
+	std::printf("\n");
 }*/
